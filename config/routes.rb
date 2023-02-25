48fb9256962b0d8b91338485553e67fff1667582
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-02-25 18:09:54 -0500

    Omit smokers resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
+  if Rails.env.test?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  resources :smokers
+  # resources :smokers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
